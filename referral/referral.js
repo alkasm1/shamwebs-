f8ccf6907c254c52df8de59ee9fd83e2d185355d
alkasm1 <shamwebs1@gmail.com>
2025-07-26 18:29:22 +0300

    Update referral.js

--- referral/referral.js
+++ referral/referral.js
@@ -1,1 +1,12 @@
+// التقاط رمز الإحالة من رابط التسجيل
+const urlParams = new URLSearchParams(window.location.search);
+const refCode = urlParams.get("ref");
 
+if (refCode) {
+  localStorage.setItem("referralCode", refCode);
+}
+
+// عند إتمام التسجيل، استرجاع رمز الإحالة وإرساله مع البيانات
+function getReferralCode() {
+  return localStorage.getItem("referralCode");
+}
